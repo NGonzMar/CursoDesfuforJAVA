722fec6472b298c36a220e93d6ce03227af6105d
p-ngonzalez <p-ngonzalez@P-NGONZALEZ.neoris.cxnetworks.net>
2023-03-27 17:55:46 +0200

    Segundo commit

--- prueba1/src/com/rf/demo/Datos.java
+++ prueba1/src/com/rf/demo/Datos.java
@@ -1,5 +1,12 @@
 package com.rf.demo;
 
 public class Datos {
+	
+	int dia = 10;
+	double nDni = 57.433.222;
+	float precio; //valores numericos con dos decimales
+	final boolean ACTIVO = true;
+			
+	precio = 135.12f;
 
 }
