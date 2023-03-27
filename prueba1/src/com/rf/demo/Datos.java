6964b10c91db2e246795f2353aafb74d2a606c01
p-ngonzalez <p-ngonzalez@P-NGONZALEZ.neoris.cxnetworks.net>
2023-03-27 18:02:42 +0200

    Primera version

--- prueba1/src/com/rf/demo/Datos.java
+++ prueba1/src/com/rf/demo/Datos.java
@@ -1,12 +1,19 @@
 package com.rf.demo;
 
 public class Datos {
-	
-	int dia = 10;
-	double nDni = 57.433.222;
-	float precio; //valores numericos con dos decimales
-	final boolean ACTIVO = true;
-			
-	precio = 135.12f;
+		
+	public static void main(String[] args) {
+		int dia = 10;
+		long nDni = 57433222;
+		float precio; //valores numericos con dos decimales
+		final boolean ACTIVO = true;
+		
+		precio = 135.12f;
+		
+		System.out.println("Dia:" + dia + 
+							"\nDni: " + nDni +
+							"\nPrecio: " + precio + 
+							"\nActivo: " + ACTIVO);
+	}
 
 }
