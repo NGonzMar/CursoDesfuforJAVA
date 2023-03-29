c57cf69d66eb27cd86cabbee22171087dfc313f5
p-ngonzalez <p-ngonzalez@P-NGONZALEZ.neoris.cxnetworks.net>
2023-03-29 10:41:19 +0200

    Ejercicio Prueba Intercambio (Genéricos)

---
+++ prueba1/src/curso/g11/genericos/PruebaIntercambio.java
@@ -0,0 +1,15 @@
+package curso.g11.genericos;
+
+public class PruebaIntercambio {
+
+    public static void main(String[] args) {
+
+        Intercambio<String> nombres = new Intercambio<String>("Nuria", "Daniel");
+
+        System.out.println("NOMBRES: \n1 - " + nombres.getUno() + "\n2 - " + nombres.getDos());
+
+        nombres.intercambiar();
+        
+        System.out.println("\nINTERCAMBIO NOMBRES: \n1 - " + nombres.getUno() + "\n2 - " + nombres.getDos());
+    }
+}
